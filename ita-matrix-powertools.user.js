ce1a2cf1a2997f95d97c2c9b6af0b53aca353fbb
bench-marc <49404117+bench-marc@users.noreply.github.com>
2019-04-14 18:31:28 +0200

    add expedia deeplinking

--- ita-matrix-powertools.user.js
+++ ita-matrix-powertools.user.js
@@ -1268,8 +1268,9 @@ function printLinksContainer(){
     }
     if(mptUsersettings["enableDeviders"]==1) printSeperator();
     /*** OTAs ***/
+    printExpedia()
     printCheapOair();
-    printPriceline ();
+    printPriceline();
     printEtraveli();
     /*** Metas ***/
     if(mptUsersettings["enableDeviders"]==1) printSeperator();
@@ -2880,6 +2881,37 @@ function printEtraveli () {
   } else {
     printUrl(ggUrl,'Seat24.de',"",extra);
   }
+}
+function printExpedia(){
+  var pax=validatePaxcount({maxPaxcount:9, countInf:true, childAsAdult:18, sepInfSeat:false, childMinAge:2});
+    if (pax===false){
+      printNotification("Error: Failed to validate Passengers in printExpedia");
+      return false;
+    }
+    let editions = [{name:"expedia.com",host:"expedia.com"},{name:"orbitz.com",host:"orbitz.com"},{name:"expedia.ca",host:"expedia.ca"},{name:"expedia.de",host:"expedia.de"},{name:"expedia.it",host:"expedia.it"},{name:"expedia.es",host:"expedia.es"},{name:"expedia.co.uk",host:"expedia.co.uk"},{name:"expedia.dk",host:"expedia.dk"},{name:"expedia.mx",host:"expedia.mx"},{name:"expedia.fi",host:"expedia.fi"},{name:"expedia.fr",host:"expedia.fr"}, {name:"expedia.no",host:"expedia.no"},{name:"expedia.nl",host:"expedia.nl"},{name:"expedia.ch",host: "expedia.ch"},{name:"expedia.se",host:"expedia.se" },{name:"expedia.at",host:"expedia.at"}];
+    let ExpediaCreateUrl = function(baseUrl) {
+      let url = "https://www." + baseUrl + "/Flight-Search-Details?action=dl&trip=MultipleDestination&cabinClass=coach&adults=" + pax.adults;
+      for (var i=0; i < currentItin["itin"].length; i++) {
+        url += "&legs%5B" + i + "%5D.departureAirport=" + currentItin["itin"][i]["orig"];
+        url += "&legs%5B" + i + "%5D.arrivalAirport=" + currentItin["itin"][i]["dest"];
+        url += "&legs%5B" + i + "%5D.departureDate=" + currentItin["itin"][i]["arr"]["year"].toString() + '-' + ("0"+currentItin["itin"][i]["dep"]["month"]).slice(-2) + '-' + ("0"+currentItin["itin"][i]["dep"]["day"]).slice(-2);
+        for (var j=0; j < currentItin["itin"][i]["seg"].length; j++) {
+          url += ("&legs%5B" + i + "%5D.segments%5B" + j + "%5D=" + currentItin["itin"][i]["seg"][j]["dep"]["year"].toString()+ '-' + ("0"+currentItin["itin"][i]["seg"][j]["dep"]["month"]).slice(-2)+ '-' +("0"+currentItin["itin"][i]["seg"][j]["dep"]["day"]).slice(-2) + '-coach-' + currentItin["itin"][i]["seg"][j]["orig"] + '-' + currentItin["itin"][i]["seg"][j]["dest"] + '-' + currentItin["itin"][i]["seg"][j]["carrier"] + '-' + currentItin["itin"][i]["seg"][j]["fnr"]).toLowerCase()
+        }
+      }
+     return url
+    }
+    var ExpediaUrl = ExpediaCreateUrl("expedia.com");
+    var container = ' <span class="pt-hover-container">[+]<span class="pt-hover-menu">';
+    container += editions.map(function(obj, i) {
+        return '<a href="' + ExpediaCreateUrl(obj.host) + '" target="_blank">' + obj.name + '</a>';
+    }).join('<br/>');
+    container += '</span></span>';
+    if (mptUsersettings["enableInlinemode"] == 1) {
+        printUrlInline(ExpediaUrl, "Expedia", "", null, container);
+    } else {
+        printUrl(ExpediaUrl, "Expedia", "", container);
+    }
 }
 /***  META ***/
 function printHipmunk(){
