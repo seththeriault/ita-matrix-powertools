317358d07dbe10daa3b663e67b3ef48ca33b5de9
Kyle <wylek1@gmail.com>
2020-02-10 12:47:33 -0800

    0.43.1

--- ita-matrix-powertools.user.js
+++ ita-matrix-powertools.user.js
@@ -2,7 +2,7 @@
 // @name ITA Matrix Powertools
 // @namespace https://github.com/adamhwang/ita-matrix-powertools
 // @description Adds new features and builds fare purchase links for ITA Matrix
-// @version 0.43.0
+// @version 0.43.1
 // @icon https://raw.githubusercontent.com/adamhwang/ita-matrix-powertools/master/icons/icon32.png
 // @require https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
 // @grant GM.getValue
@@ -1167,7 +1167,7 @@ const appSettings = {
   scriptEngine:
     typeof GM === "undefined" || typeof GM.info === "undefined" ? 0 : 1, // 0 - console mode, 1 - tamper or grease mode
   itaLanguage: "en",
-  version: "0.43.0",
+  version: "0.43.1",
   retrycount: 1,
   laststatus: "",
   scriptrunning: 1,
@@ -2976,7 +2976,7 @@ function printBA() {
       }
     }
     url +=
-      "&p=EUR6666.66&e=FP&c=" +
+      "&p=EUR6666.66&e=SL&c=" +
       cabins[_settings_appSettings__WEBPACK_IMPORTED_MODULE_0__[/* default */ "a"].cabin === "Auto" ? mincabin : Object(_settings_appSettings__WEBPACK_IMPORTED_MODULE_0__[/* getForcedCabin */ "c"])()] +
       "&source=FareQuoteEmail&isEmailHBOFareQuote=false";
     return url;
