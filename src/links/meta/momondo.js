7375ab18128093dfb88dcfd2e4cd1c4c13aebbcc
Adam Hwang <github@adamhwang.com>
2020-02-21 11:25:54 -0600

    Added segments and children to momondo

--- src/links/meta/momondo.js
+++ src/links/meta/momondo.js
@@ -1,83 +1,135 @@
-import mptSettings, { getForcedCabin } from "../../settings/appSettings";
-import { currentItin } from "../../parse/itin";
-import { registerLink } from "../../print/links";
+import { getCabin } from "../../settings/appSettings";
+import mptUserSettings from "../../settings/userSettings";
+import { registerLink, validatePaxcount } from "../../print/links";
+import { currentItin, getCurrentSegs } from "../../parse/itin";
+import { printNotification, to2digits } from "../../utils";
 
-const MomondoEditions = [
-  { name: "Momondo.com", host: "Momondo.com" },
-  { name: "Momondo.de", host: "Momondo.de" },
-  { name: "Momondo.it", host: "Momondo.it" },
-  { name: "Momondo.es", host: "Momondo.es" },
-  { name: "Momondo.co.uk", host: "Momondo.co.uk" },
-  { name: "Momondo.dk", host: "Momondo.dk" },
-  { name: "Momondo.mx", host: "Momondo.mx" },
-  { name: "Momondo.fi", host: "Momondo.fi" },
-  { name: "Momondo.fr", host: "Momondo.fr" },
-  { name: "Momondo.no", host: "Momondo.no" },
-  { name: "Momondo.nl", host: "Momondo.nl" },
-  { name: "Momondo.pt", host: "Momondo.pt" },
-  { name: "Momondo.se", host: "Momondo.se" },
-  { name: "Momondo.ru", host: "Momondo.ru" }
+const editions = [
+  { name: "Momondo.ar", host: "www.momondo.com.ar" },
+  { name: "Momondo.at", host: "www.momondo.at" },
+  { name: "Momondo.au", host: "www.momondo.com.au" },
+  { name: "Momondo.be", host: "www.momondo.be" },
+  { name: "Momondo.br", host: "www.momondo.com.br" },
+  { name: "Momondo.by", host: "www.momondo.by" },
+  { name: "Momondo.ca", host: "www.momondo.ca" },
+  { name: "Momondo.ch", host: "www.momondo.ch" },
+  { name: "Momondo.cl", host: "www.momondo.cl" },
+  { name: "Momondo.cn", host: "www.cn.momondo.com" },
+  { name: "Momondo.co", host: "www.momondo.com.co" },
+  { name: "Momondo.cz", host: "www.momondo.cz" },
+  { name: "Momondo.de", host: "www.momondo.de" },
+  { name: "Momondo.dk", host: "www.momondo.dk" },
+  { name: "Momondo.ee", host: "www.momondo.ee" },
+  { name: "Momondo.es", host: "www.momondo.es" },
+  { name: "Momondo.fi", host: "www.momondo.fi" },
+  { name: "Momondo.fr", host: "www.momondo.fr" },
+  { name: "Momondo.hk", host: "www.momondo.hk" },
+  { name: "Momondo.ie", host: "www.momondo.ie" },
+  { name: "Momondo.in", host: "www.momondo.in" },
+  { name: "Momondo.it", host: "www.momondo.it" },
+  { name: "Momondo.kz", host: "www.momondo.kz" },
+  { name: "Momondo.mx", host: "www.momondo.mx" },
+  { name: "Momondo.nl", host: "www.momondo.nl" },
+  { name: "Momondo.no", host: "www.momondo.no" },
+  { name: "Momondo.nz", host: "www.momondo.co.nz" },
+  { name: "Momondo.om", host: "www.momondo.com" },
+  { name: "Momondo.pe", host: "www.momondo.com.pe" },
+  { name: "Momondo.pl", host: "www.momondo.pl" },
+  { name: "Momondo.pt", host: "www.momondo.pt" },
+  { name: "Momondo.ro", host: "www.momondo.ro" },
+  { name: "Momondo.ru", host: "www.momondo.ru" },
+  { name: "Momondo.se", host: "www.momondo.se" },
+  { name: "Momondo.tr", host: "www.momondo.com.tr" },
+  { name: "Momondo.tw", host: "www.momondo.tw" },
+  { name: "Momondo.ua", host: "www.momondo.ua" },
+  { name: "Momondo.uk", host: "www.momondo.co.uk" },
+  { name: "Momondo.za", host: "www.momondo.co.za" }
 ];
 
-function printMomondo() {
+var cabins = ["economy", "premium", "business", "first"];
+
+function print(method) {
   //example https://www.Momondo.ru/flightsearch/?...false&NA=false
   //pax # &AD=2&CA=0,8 – not working with children (total amount of adults + kids goes to adult)
+  // method: 0 = based on leg; 1 = based on segment
+  const segs = !method ? currentItin.itin : getCurrentSegs();
+  if (method && currentItin.itin.length === segs.length) return;
 
-  var momondoTravelClass = ["economy", "premium", "business", "first"];
-  var mincabin = 3;
+  var pax = validatePaxcount({
+    maxPaxcount: 9,
+    countInf: false,
+    childAsAdult: 12,
+    sepInfSeat: true,
+    childMinAge: 2
+  });
+  if (!pax) {
+    printNotification("Error: Failed to validate Passengers in printOvago");
+    return;
+  }
 
-  var MomondoCreateUrl = function(host) {
-    var MomondoUrl = "https://www." + host + "/flight-search/";
-    for (var i = 0; i < currentItin.itin.length; i++) {
-      MomondoUrl +=
-        currentItin.itin[i].orig +
-        "-" +
-        currentItin.itin[i].dest +
-        "/" +
-        currentItin.itin[i].dep.year +
-        "-" +
-        ("0" + currentItin.itin[i].dep.month).slice(-2) +
-        "-" +
-        ("0" + currentItin.itin[i].dep.day).slice(-2) +
-        "/";
+  const cabin =
+    cabins[getCabin(Math.min(...getCurrentSegs().map(seg => seg.cabin)))];
 
-      for (var j = 0; j < currentItin.itin[i].seg.length; j++) {
-        // check the min cabin:
-        if (currentItin.itin[i].seg[j].cabin < mincabin) {
-          mincabin = currentItin.itin[i].seg[j].cabin;
-        }
+  var createUrl = function(host) {
+    let url =
+      `https://${host}/flight-search/` +
+      segs
+        .map(
+          seg =>
+            `${seg.orig}-${seg.dest}/${seg.dep.year}-${to2digits(
+              seg.dep.month
+            )}-${to2digits(seg.dep.day)}`
+        )
+        .join("/");
+
+    if (pax.adults > 1 || pax.children.length || pax.infSeat || pax.infLap) {
+      url += `/${currentItin.numPax}adults`;
+    }
+
+    if (pax.children.length || pax.infSeat || pax.infLap) {
+      url += "/children";
+      for (let i = 0; i < pax.infSeat; i++) {
+        url += "-1S";
+      }
+      for (let i = 0; i < pax.infLap; i++) {
+        url += "-1L";
+      }
+      for (let i = 0; i < pax.children.length; i++) {
+        url += "-11";
       }
     }
 
-    // Add travel class to URL:
-    MomondoUrl +=
-      momondoTravelClass[
-        mptSettings.cabin === "Auto" ? mincabin : getForcedCabin()
-      ] + "/";
-    // Add passenger info to URL:
-    MomondoUrl += currentItin.numPax + "adults";
-    return MomondoUrl;
+    url += "/" + cabin;
+
+    return url;
   };
 
-  var MomondoUrl = MomondoCreateUrl("Momondo.com");
-  var MomondoExtra =
+  var url = createUrl("www.momondo.com");
+  var extra =
     ' <span class="pt-hover-container">[+]<span class="pt-hover-menu">';
-  MomondoExtra += MomondoEditions.map(function(obj, i) {
-    return (
-      '<a href="' +
-      MomondoCreateUrl(obj.host) +
-      '" target="_blank">' +
-      obj.name +
-      "</a>"
-    );
-  }).join("<br/>");
-  MomondoExtra += "</span></span>";
+  extra += editions
+    .map(function(obj, i) {
+      return (
+        '<a href="' +
+        createUrl(obj.host) +
+        '" target="_blank">' +
+        obj.name +
+        "</a>"
+      );
+    })
+    .join("<br/>");
+  extra += "</span></span>";
 
   return {
-    url: MomondoUrl,
+    url,
     title: "Momondo",
-    extra: MomondoExtra
+    desc:
+      mptUserSettings.language == "de"
+        ? `Benutze ${segs.length} Segment(e)`
+        : `Based on ${segs.length} segment(s)`,
+    extra
   };
 }
 
-registerLink("meta", printMomondo);
+registerLink("meta", () => print(0));
+registerLink("meta", () => print(1));
