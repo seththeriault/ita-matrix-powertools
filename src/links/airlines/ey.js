1f67ef8b7f76cc30a574d7d4f1ad86f4d2b3d844
Adam Hwang <github@adamhwang.com>
2020-02-24 15:14:22 -0600

    Added Etihad

---
+++ src/links/airlines/ey.js
@@ -0,0 +1,76 @@
+import mptUserSettings from "../../settings/userSettings";
+import {
+  printNotification,
+  to2digits,
+  inArray,
+  to4digitTime
+} from "../../utils";
+import { registerLink } from "../../print/links";
+import { currentItin } from "../../parse/itin";
+import { getCabin } from "../../settings/appSettings";
+import { validatePax } from "..";
+
+const cabins = ["ECONOMY", "ECONOMY", "BUSINESS", "FIRST"];
+
+function printEY() {
+  if (
+    !mptUserSettings.showAllAirlines &&
+    !inArray("EY", currentItin.carriers)
+  ) {
+    return;
+  }
+
+  var createUrl = function() {
+    var pax = validatePax({
+      maxPaxcount: 9,
+      countInf: false,
+      childAsAdult: 12,
+      sepInfSeat: false,
+      childMinAge: 2
+    });
+    if (!pax) {
+      printNotification("Error: Failed to validate Passengers in printEY");
+      return;
+    }
+
+    let url =
+      "https://booking.etihad.com/SSW2010/EYEY/webqtrip.html?journeySpan=MC";
+    url += `&numAdults=${pax.adults}`;
+    url += `&numChildren=${pax.children.length}`;
+    url += `&numInfants=${pax.infLap}`;
+    url += `&advertisedFare=${currentItin.price}`;
+    url += `&currency=${currentItin.cur || "USD"}`;
+
+    let segnum = 0;
+    currentItin.itin.forEach((itin, legnum) => {
+      itin.seg.forEach(seg => {
+        url += `&ms[${segnum}].from=${seg.orig}`;
+        url += `&ms[${segnum}].to=${seg.dest}`;
+        url += `&ms[${segnum}].departure=${seg.dep.year}-${to2digits(
+          seg.dep.month
+        )}-${to2digits(seg.dep.day)}T${to4digitTime(seg.dep.time24)}`;
+        url += `&ms[${segnum}].arrival=${seg.arr.year}-${to2digits(
+          seg.arr.month
+        )}-${to2digits(seg.arr.day)}T${to4digitTime(seg.arr.time24)}`;
+        url += `&ms[${segnum}].leg=${legnum}`;
+        url += `&ms[${segnum}].cbnClass=${cabins[getCabin(seg.cabin)]}`;
+
+        segnum++;
+      });
+    });
+
+    return url;
+  };
+
+  var url = createUrl();
+  if (!url) {
+    return;
+  }
+
+  return {
+    url,
+    title: "Etihad"
+  };
+}
+
+registerLink("airlines", printEY);
