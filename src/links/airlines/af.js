648e47f0e7e4d985cf8a356367418d0bcb98873b
Adam Hwang <github@adamhwang.com>
2020-02-26 20:56:38 -0600

    Fixed AF deeplinks

--- src/links/airlines/af.js
+++ src/links/airlines/af.js
@@ -1,142 +1,79 @@
-import mptSettings, { getForcedCabin } from "../../settings/appSettings";
+import { getCabin } from "../../settings/appSettings";
 import mptUserSettings, { registerSetting } from "../../settings/userSettings";
-import { printNotification } from "../../utils";
-import { validatePax, register } from "..";
-import { currentItin } from "../../parse/itin";
+import {
+  printNotification,
+  to2digits,
+  to4digitTime,
+  to4digits
+} from "../../utils";
+import { validatePax, register, anyCarriers } from "..";
+import { getCurrentSegs } from "../../parse/itin";
 
-const afEditions = [
-  { value: "DE/de", name: "Germany / Deutsch" },
-  { value: "DE/en", name: "Germany / English" },
-  { value: "FR/en", name: "France / English" },
-  { value: "FI/en", name: "Finland / English" },
-  { value: "FR/fr", name: "France / French" },
-  { value: "NL/en", name: "Netherlands / English" },
-  { value: "GB/en", name: "United Kingdom / English" },
-  { value: "US/en", name: "US / English" }
+const editions = [
+  { name: "Brazil", value: "www.airfrance.com.br", country: "BR" },
+  { name: "Finland", value: "www.airfrance.fi", country: "FI" },
+  { name: "France", value: "www.airfrance.fr", country: "FR" },
+  { name: "Germany", value: "www.airfrance.de", country: "DE" },
+  { name: "Italy", value: "www.airfrance.it", country: "IT" },
+  { name: "Netherlands", value: "www.airfrance.nl", country: "NL" },
+  { name: "Spain", value: "www.airfrance.es", country: "ES" },
+  { name: "United Kingdom", value: "www.airfrance.co.uk", country: "GB" },
+  { name: "United States", value: "www.airfrance.us", country: "US" }
 ];
 
-function printAF() {
-  var createUrl = function(edition) {
-    if (
-      !mptUserSettings.showAllAirlines &&
-      !(
-        currentItin.itin &&
-        currentItin.itin.length == 2 &&
-        currentItin.itin[0].orig == currentItin.itin[1].dest &&
-        currentItin.itin[0].dest == currentItin.itin[1].orig
+const cabins = ["ECONOMY", "W", "C", "F"];
+
+function print() {
+  if (!anyCarriers("AF", "KL")) {
+    return;
+  }
+
+  var pax = validatePax({
+    maxPaxcount: 9,
+    countInf: true,
+    childAsAdult: 12,
+    sepInfSeat: false,
+    childMinAge: 2
+  });
+  if (!pax) {
+    printNotification("Error: Failed to validate Passengers in printAF");
+    return;
+  }
+
+  const segs = getCurrentSegs();
+  const cabin = cabins[getCabin(Math.max(...segs.map(seg => seg.cabin)))];
+
+  const createUrl = function(edition) {
+    const country = editions.find(e => e.value === edition).country;
+    return (
+      `https://${edition}/ams/exchange?language=${mptUserSettings.language}&country=${country}&target=` +
+      encodeURIComponent(
+        `/search/summary?deviationValue=5&connections=${segs
+          .map(
+            seg =>
+              `${seg.orig}:${seg.dep.year}${to2digits(
+                seg.dep.month
+              )}${to2digits(seg.dep.day)}@${to4digitTime(seg.dep.time24)}:${
+                seg.carrier
+              }${to4digits(seg.fnr)}:${seg.bookingclass}:${seg.farebase}:${
+                cabins[getCabin(seg.cabin)]
+              }>${seg.dest}`
+          )
+          .join("-")}&cabinClass=${cabin}&pax=${pax.adults}:0:${
+          pax.children.length
+        }:${pax.infLap}`
       )
-    ) {
-      return;
-    }
-
-    var cabins = ["Y", "W", "C", "F"];
-    var mincabin = 3;
-    var afUrl =
-      "https://www.airfrance.com/" +
-      edition +
-      "/local/process/standardbooking/DisplayUpsellAction.do?calendarSearch=1&subCabin=MCHER&typeTrip=2";
-    for (var i = 0; i < currentItin.itin.length; i++) {
-      if (i == 0) {
-        afUrl += "&from=" + currentItin.itin[i].orig;
-        afUrl += "&to=" + currentItin.itin[i].dest;
-        afUrl +=
-          "&outboundDate=" +
-          currentItin.itin[i].dep.year +
-          "-" +
-          ("0" + currentItin.itin[i].dep.month).slice(-2) +
-          "-" +
-          ("0" + currentItin.itin[i].dep.day).slice(-2);
-        afUrl +=
-          "&firstOutboundHour=" +
-          ("0" + currentItin.itin[i].dep.time).slice(-5);
-
-        let flights = "";
-        for (var j = 0; j < currentItin.itin[i].seg.length; j++) {
-          if (j > 0) flights += "|";
-          flights +=
-            currentItin.itin[i].seg[j].carrier +
-            ("000" + currentItin.itin[i].seg[j].fnr).slice(-4);
-        }
-        afUrl += "&flightOutbound=" + flights;
-      } else if (i == 1) {
-        afUrl +=
-          "&inboundDate=" +
-          currentItin.itin[i].dep.year +
-          "-" +
-          ("0" + currentItin.itin[i].dep.month).slice(-2) +
-          "-" +
-          ("0" + currentItin.itin[i].dep.day).slice(-2);
-        afUrl +=
-          "&firstInboundHour=" + ("0" + currentItin.itin[i].dep.time).slice(-5);
-
-        let flights = "";
-        for (var j = 0; j < currentItin.itin[i].seg.length; j++) {
-          if (j > 0) flights += "|";
-          flights +=
-            currentItin.itin[i].seg[j].carrier +
-            ("000" + currentItin.itin[i].seg[j].fnr).slice(-4);
-          if (currentItin.itin[i].seg[j].cabin < mincabin) {
-            mincabin = currentItin.itin[i].seg[j].cabin;
-          }
-        }
-        afUrl += "&flightInbound=" + flights;
-      }
-    }
-    afUrl +=
-      "&cabin=" +
-      cabins[mptSettings.cabin === "Auto" ? mincabin : getForcedCabin()];
-    var pax = validatePax({
-      maxPaxcount: 9,
-      countInf: true,
-      childAsAdult: 18,
-      sepInfSeat: false,
-      childMinAge: 2
-    });
-    if (!pax) {
-      printNotification("Error: Failed to validate Passengers in printAF");
-      return;
-    }
-    var tmpPax = { c: 0, y: 0 };
-    for (i = 0; i < pax.children.length; i++) {
-      if (pax.children[i] > 11) {
-        tmpPax.y++;
-      } else {
-        tmpPax.c++;
-      }
-    }
-    var curPax = 0;
-    afUrl += "&nbAdults=" + pax.adults;
-    for (i = 0; i < pax.adults; i++) {
-      afUrl += "&paxTypoList=ADT";
-      curPax++;
-    }
-    afUrl += "&nbEnfants=" + tmpPax.y;
-    for (i = 0; i < tmpPax.y; i++) {
-      afUrl += "&paxTypoList=YTH_MIN";
-      curPax++;
-    }
-    afUrl += "&nbChildren=" + tmpPax.c;
-    for (i = 0; i < tmpPax.y; i++) {
-      afUrl += "&paxTypoList=CHD";
-      curPax++;
-    }
-    afUrl += "&nbBebes=" + pax.infLap;
-    for (i = 0; i < pax.infLap; i++) {
-      afUrl += "&paxTypoList=INF";
-      curPax++;
-    }
-    afUrl += "&nbPassenger=" + curPax + "&nbPax=" + curPax;
-    return afUrl;
+    );
   };
-  // get edition
-  var edition = mptUserSettings.afEdition;
-  var url = createUrl(edition);
+
+  var url = createUrl(mptUserSettings.afEdition2);
   if (!url) {
     return;
   }
+
   var extra =
     ' <span class="pt-hover-container">[+]<span class="pt-hover-menu">';
-  extra += afEditions
+  extra += editions
     .map(function(obj, i) {
       return (
         '<a href="' +
@@ -156,5 +93,5 @@ function printAF() {
   };
 }
 
-register("airlines", printAF);
-registerSetting("Air France", "afEdition", afEditions, "US/en");
+register("airlines", print);
+registerSetting("Air France", "afEdition2", editions, "www.airfrance.us");
