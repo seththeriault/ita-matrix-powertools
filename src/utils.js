3fc7d244c4d797d86b499dfa63a377d54951c1bb
Adam Hwang <github@adamhwang.com>
2020-02-27 21:21:58 -0600

    Added GUID generator

--- src/utils.js
+++ src/utils.js
@@ -88,6 +88,14 @@ export function to4digitTime(time) {
   return to4digits(time.replace(":", ""));
 }
 
+export function uuidv4() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
+
 export function monthnumberToName(month) {
   var monthnames = [
     "JAN",
